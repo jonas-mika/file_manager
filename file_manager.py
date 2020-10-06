38326ec18788bc2053c8a5e968d6d71fc0d922ad
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2020-10-06 20:27:20 +0200

    Formatting

--- file_manager.py
+++ file_manager.py
@@ -20,167 +20,11 @@ from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-# Config
+# Open Configuration File 
 with open('./sorting.json', 'r') as f:
 	CONFIG = json.load(f)
 
-#Sorting depending on File
-# def sort_datatype(path, file):
-# 	i = 1
-# 	source_path = "{}/{}".format(path, file)
-# 	file_name, file_ext = os.path.splitext(file)
-
-# 	if file_ext in filetypes["video"]:
-# 		try: 
-# 			os.mkdir("{}/video".format(folder_destination)) 
-# 		except: None
-		
-# 		new_path = "{}/video/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <videos>")
-
-# 	#photos
-# 	elif file_ext in filetypes["photo"]:
-# 		try:
-# 			os.mkdir("{}/photos".format(folder_destination))
-# 		except: None
-
-# 		new_path = "{}/photos/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <photos>")
-
-# 	#audio
-# 	elif file_ext in filetypes["audio"]:
-# 		try:
-# 			os.mkdir("{}/audio".format(folder_destination))
-# 		except: None
-
-# 		new_path = "{}/audio/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <audio>")
-
-# 	#creativity
-# 	elif file_ext in filetypes["creativity"]:
-# 		try:
-# 			os.mkdir("{}/creativity".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/creativity/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <creativity>")
-
-# 	#productivity
-# 	elif file_ext in filetypes["productivity"]:
-# 		try:
-# 			os.mkdir("{}/productivity".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/productivity/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <productivity>")
-
-# 	#programming
-# 	elif file_ext in filetypes["programming"]:
-# 		try:
-# 			os.mkdir("{}/programming".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/programming/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <programming>")
-
-# 	#installers
-# 	elif file_ext in filetypes["installers"]:
-# 		try:
-# 			os.mkdir("{}/installer".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/installer/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <installer>")
-
-# 	#data_analysis
-# 	elif file_ext in filetypes["datascience"]:
-# 		try:
-# 			os.mkdir("{}/data_analysis".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/data_analysis/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <data_analysis>")
-
-# 	#web
-# 	elif file_ext in filetypes["web"]:
-# 		try:
-# 			os.mkdir("{}/web".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/web/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <web>")
-
-# 	else:
-# 		try:
-# 			os.mkdir("{}/other".format(folder_destination)) 
-# 		except: None
-# 		new_path = "{}/other/{}".format(folder_destination, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> moved to <other>")
-
-# 	#renaming file
-# 	new_name = rename(file)
-# 	name, ext = os.path.splitext(new_name)
-# 	root = "/".join(new_path.split("/")[:-1])
-	
-# 	renamed_path = "{}/{}".format(root, new_name)
-	
-# 	#check for duplicates
-# 	file_exists = os.path.exists(renamed_path)
-	
-# 	while file_exists:
-# 		i += 1
-# 		renamed_path = root + "/" + name + str(i) + ext
-# 		file_exists = os.path.exists(renamed_path)
-		
-# 	os.rename(new_path, renamed_path)
-
-# def sort_itu(path,file):
-# 	i = 1
-# 	source_path = "{}/{}".format(path, file)
-# 	file_name, file_ext = os.path.splitext(file)
-
-# 	course, week = "".join(file_name.split(" ")[1:]).split(",")[0:2]
-	
-# 	if course == str(1):
-# 		try:
-# 			os.mkdir("{}/Week {}".format(IDSP, week))
-# 		except: None
-# 		new_path = "{}/Week {}/{}".format(IDSP, week, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> succesfully moved to IDSP")
-
-# 	elif course == str(2):
-# 		try:
-# 			os.mkdir("{}/Week {}".format(MATH, week))
-# 		except: None
-# 		new_path = "{}/Week {}/{}".format(MATH, week, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> succesfully moved to Linear Algebra and Optimisation")
-
-# 	elif course == str(3):
-# 		try:
-# 			os.mkdir("{}/Week {}".format(RBS, week))
-# 		except: None
-# 		new_path = "{}/Week {}/{}".format(RBS, week, file)
-# 		os.rename(source_path, new_path)
-# 		print(">>> succesfully moved to RBS")
-
-# 	else: print("Something went wrong")
-
-# 	#renaming file
-# 	new_name = rename(file)
-# 	print(new_name)
-# 	root = "/".join(new_path.split("/")[:-1])
-	
-# 	renamed_path = "{}/{}".format(root, new_name)
-# 	print(renamed_path)
-		
-# 	os.rename(new_path, renamed_path)
-
+# Move and rename file
 def move_file(src, file, dest):
 	try: os.makedirs(f'{dest}')
 	except: None
@@ -190,9 +34,6 @@ def move_file(src, file, dest):
 
 #Driver Code, when watchdogs recognizes change in tracked folders
 def on_modified(event):
-	# Rule of thumb, don't put functions in loop signature, it can make it significant more slot.
-	# I don't think it makes a big impact here, but it will in other languages.
-
 	try:
 		file_names = os.listdir(event.src_path)
 	except:
@@ -208,15 +49,6 @@ def on_modified(event):
 					move_file(event.src_path, file, dest_filtered)
 			except: None
 
-		# src = f'{event.src_path}/{file}'
-
-		# if re.match("^itu", file):
-		# 	# sort_itu(src, CONFIG[])
-		# 	pass
-		# elif re.match("^Screenshot", file):
-		# 	sort_screenshot(src, CONFIG['img_dest'])
-		# else:
-		# 	sort_datatype(event.src_path, file)
 
 def main():
 	#initialize event handler as instance of class "FileSystemEventHandler"
